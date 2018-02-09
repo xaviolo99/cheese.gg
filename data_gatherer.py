b2d3ac5d5513718f32e8b277b3c95f4a4e60cc48
Xavi Reyes <xavi.reyes999@gmail.com>
2018-02-09 14:12:49 +0100

    Update data_gatherer.py

--- data_gatherer.py
+++ data_gatherer.py
@@ -14,7 +14,7 @@ print("cheese.gg Recursive Data Gatherer v0.1, by xaviolo99")
 
 apiKey = "" #If it doesnt work, create a new one
 beginTime = str( ( int(time.time())-1209600 )*1000 ) #https://www.epochconverter.com/ (1209600 seconds is 14 days)
-delay = 120/(100*0.95) #seconds in 2 mins/(number of requests allowed per 2 minutes * penalty to avoid rate limiting)
+delay = 120/(100*0.95) #seconds in 2 mins/(number of requests allowed per 2 minutes * penalty to avoid exceeding rate limit)
 
 iSumm = ""#accountId
 while iSumm == "":
